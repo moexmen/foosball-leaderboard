a90ab63309517d74e241edd3dfc8ddd7858fd461
Hansel <heyhansel@outlook.com>
2016-04-01 11:34:09 +0800

    Added player functions

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,43 @@
 class PlayersController < ApplicationController
+	def index
+		@players = Player.all
+	end
+
+	def show
+		@player = Player.find(params[:id])
+	end
+
+	def new
+		@player = Player.new
+	end
+
+	def create
+		@player = Player.new(player_params)
+		@player.save
+
+		redirect_to players_path
+	end
+
+	def edit
+	  @player = Player.find(params[:id])
+	end
+
+	def update
+		@player = Player.find(params[:id])
+ 
+	  @player.update(player_params)
+	  redirect_to @player
+	end
+
+	def destroy
+	  @player = Player.find(params[:id])
+	  @player.destroy
+	 
+	  redirect_to players_path
+	end
+
+private
+	def player_params
+		params.require(:player).permit(:name, :alias)
+	end
 end
