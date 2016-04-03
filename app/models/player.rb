0921be3295dd845e054c510ce895f0091c4786cc
Ivan Tan <ivantjh97@gmail.com>
2016-04-03 23:14:29 +0800

    Generate score record for new player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,11 @@
 class Player < ActiveRecord::Base
+  has_one :Score, dependent: :destroy
+
+  after_create do
+    # generate score record
+    Score.create(wins: 0, loses: 0, points: 0, goals: 0, wRatio: 0, pullUps: 0, player_id: self.id)
+    puts "[SERVER] Generated new score record for player #{self.id}"
+  end
+
+
 end
