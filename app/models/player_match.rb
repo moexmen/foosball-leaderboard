bb6181e1cbf5d781ecd052ede9c36271747b9f06
Ivan Tan <ivantjh97@gmail.com>
2016-04-03 20:52:50 +0800

    Re-organized score and playerMatch models

--- app/models/player_match.rb
+++ app/models/player_match.rb
@@ -3,47 +3,7 @@ class PlayerMatch < ActiveRecord::Base
   after_update 'update_player_score'
 
   def update_player_score
-    # Update score for individual player
-    match = Match.find(self.match_id)
-    score = Score.where(player_id: self.player_id).take!
-
-    puts 'ADDING MATCH'
-    puts 'SCORE ID: ' + score.id.to_s
-    puts 'WINS: ' + score.wins.to_s
-    puts 'LOSES: ' + score.loses.to_s
-    puts 'POINTS: ' + score.points.to_s
-    puts 'GOALS: ' + score.goals.to_s
-    puts 'PULLUPS' + score.pullUps.to_s
-    puts 'WRATIO' + score.wRatio.to_s
-
-    # update wins and points
-    (match.winner == team) ? score.wins += 1 : score.loses += 1
-    (match.winner == team) ? score.points += 3 : score.points += 1
-
-    # update goals
-    (team == 'r') ? score.goals += match.redGoal : score.goals += match.blueGoal
-
-    # update pullUps
-    no_pull_up = (match.winner == 'r') ? match.redGoal - match.blueGoal : match.blueGoal - match.redGoal
-    score.pullUps += no_pull_up if match.winner != team
-
-    # update wRatio
-    new_win_ratio = score.wins.fdiv(score.wins + score.loses)
-    score.wRatio = sprintf '.10f', new_win_ratio
-
-    # save scoreboard
-    score.save
-
-    puts 'AFTER ADDING MATCH MATCH'
-    puts 'SCORE ID: ' + score.id.to_s
-    puts 'WINS: ' + score.wins.to_s
-    puts 'LOSES: ' + score.loses.to_s
-    puts 'POINTS: ' + score.points.to_s
-    puts 'GOALS: ' + score.goals.to_s
-    puts 'PULLUPS' + score.pullUps.to_s
-    puts 'WRATIO' + score.wRatio.to_s
-
-    puts "[SERVER] Updated scoreboard of player #{self.player_id}"
+    Score.update_player_score(self.match_id, self)
   end
 
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -18,17 +18,33 @@ class Score < ActiveRecord::Base
     new_win_ratio = score.wins.fdiv(score.wins + score.loses)
     score.wRatio = sprintf '.10f', new_win_ratio
 
-    puts 'REMOVE MATCH'
-    puts 'SCORE ID: ' + score.id.to_s
-    puts 'WINS: ' + score.wins.to_s
-    puts 'LOSES: ' + score.loses.to_s
-    puts 'POINTS: ' + score.points.to_s
-    puts 'GOALS: ' + score.goals.to_s
-    puts 'PULLUPS: ' + score.pullUps.to_s
-    puts 'WRATIO: ' + score.wRatio.to_s
-
     score.save
     puts "[SERVER] Removed match #{match.id} from player #{player_match.player_id}'s scoreboard"
+  end
+
+  def self.update_player_score(match_id, player_match)
+    # Update score for individual player
+    match = Match.find(match_id)
+    score = Score.where(player_id: player_match.player_id).take!
+
+    # update wins and points
+    (match.winner == player_match.team) ? score.wins += 1 : score.loses += 1
+    (match.winner == player_match.team) ? score.points += 3 : score.points += 1
+
+    # update goals
+    (player_match.team == 'r') ? score.goals += match.redGoal : score.goals += match.blueGoal
+
+    # update pullUps
+    no_pull_up = (match.winner == 'r') ? match.redGoal - match.blueGoal : match.blueGoal - match.redGoal
+    score.pullUps += no_pull_up if match.winner != player_match.team
+
+    # update wRatio
+    new_win_ratio = score.wins.fdiv(score.wins + score.loses)
+    score.wRatio = sprintf '.10f', new_win_ratio
+
+    # save scoreboard
+    score.save
+    puts "[SERVER] Updated scoreboard of player #{player_match.player_id}"
   end
 
 end
