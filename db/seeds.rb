3a3e180c1b6cc94e161831842645f03325af2d17
handshou <heyhansel@outlook.com>
2016-04-04 10:49:17 +0800

    Mini commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-def rand_gen(n)
-	1 + rand(n)
+def rand_gen
+	1 + rand(180)
 end
 
-player_list = [
-	["Toh Weiqing", "WQ"],
-	["Soh Yu Ming", "SYM"],
-	["Benny Ou", "BO"],
-	["Brian Koh", "BK"],
-	["Fong Li Heng", "FLH"],
-	["Eugene Chow", "EC"],
-	["Soedarsono", "SD"],
-	["Evelyn Lee", "EL"],
-	["Ben Leong", "BL"],
-	["Luo Yanjie", "LY"],
-	["Abel Tay", "AT"]
-]
+=begin
+	points = 3 * wins + 1 * loses
+	wRatio = win/win + lose
+=end
 
-player_list.each do |name, nick|
-	Player.create(name: name, alias: nick)
-end
-
-5.times do |i|
-	redAtt = rand_gen(11)
-	redDef = rand_gen(11)
-	blueAtt = rand_gen(11)
-	blueDef = rand_gen(11)
-	redGoal = rand_gen(10)
-	blueGoal = rand_gen(10)
-	winner = redGoal > blueGoal ? 0 : 1
-
-	Match.create(redAtt: redAtt, redDef: redDef, blueAtt: blueAtt, blueDef: blueDef, 
-		redGoal: redGoal, blueGoal: blueGoal, winner: winner)
+10.times do |i|
+	player_id = i
+	actual_pid = (i + 1).to_s
+	wins = rand_gen
+	loses = rand_gen
+	points = 3 * wins + 1 * loses
+	goals = rand_gen
+	wRatio = wins.fdiv(wins + loses)
+	display_wRatio = sprintf "%.10f", wRatio
+	pullUps = rand_gen
+	
+	Score.create(player_id: actual_pid, wins: wins, loses: loses, points: points, 
+		goals: goals, wRatio: display_wRatio, pullUps: pullUps)
 end
