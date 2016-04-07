d49c2832d5afd48dd4a42b97767c9c4f6918c294
Ivan Tan <ivantjh97@gmail.com>
2016-04-07 18:37:59 +0800

    Added validations for matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,10 +1,4 @@
 class MatchesController < ApplicationController
-  include ActiveModel::Validations
-
-  validate do
-    errors.add(:base, 'Blue team cannot win')
-  end
-
   def index
     active_tab
     @matches = Match.all
@@ -14,44 +8,42 @@ class MatchesController < ApplicationController
     active_tab
     @match = Match.new
     @players = Player.where(active: true)
+    @error = check_for_error_params
   end
 
   def edit
-    # called upon submitting GET request
     active_tab
     @match = Match.find(params[:id])
     @players = Player.where(active: true)
+    @error = check_for_error_params
   end
 
   def create
-    @errors = validation
-    puts 'LOOK HERE GUYS'
-    puts @errors
+    @match = Match.new(redGoal: match_params[:redGoal], blueGoal: match_params[:blueGoal], winner: match_params[:winner])
 
-    if @errors.empty?
+    if validation.empty?
       update_players_for_match
-      @match = Match.create(redGoal: match_params[:redGoal], blueGoal: match_params[:blueGoal], winner: match_params[:winner])
+      @match.save
       redirect_to matches_path
     else
-      @errors
-      redirect_to new_match_path
+      redirect_to new_match_path(validation)
     end
   end
 
   def update
-    # called upon submitting a PATCH request through edit
     @match = Match.find(params[:id])
-    update_players_for_match
-    update_hash = {
-        redGoal: match_params[:redGoal],
-        blueGoal: match_params[:blueGoal],
-        winner: match_params[:winner]
-    }
 
-    if @match.update(update_hash)
+    if validation.empty?
+      update_hash = {
+          redGoal: match_params[:redGoal],
+          blueGoal: match_params[:blueGoal],
+          winner: match_params[:winner]
+      }
+      update_players_for_match
+      @match.update(update_hash)
       redirect_to matches_path
     else
-      render 'edit'
+      redirect_to action: :edit, id: @match.id, params: validation
     end
   end
 
@@ -72,39 +64,40 @@ class MatchesController < ApplicationController
     params.permit(:redAtt, :redDef, :blueAtt, :blueDef, :redGoal, :blueGoal, )
   end
 
-  private
   def update_players_for_match
     @match.add_players_for_match(match_params[:redAtt], match_params[:redDef], match_params[:blueAtt], match_params[:blueDef])
   end
 
-  private
   def validation
-    players = [match_params[:redAtt], match_params[:redDef], match_params[:blueAtt], match_params[:blueDef]]
+    @goal_check = true
+    @player_check = false
+
+    check_players([match_params[:redAtt], match_params[:redDef], match_params[:blueAtt], match_params[:blueDef]])
+    check_goal(match_params[:redGoal].to_i, match_params[:blueGoal].to_i)
+
     error = {}
-    error[:goal] = 'Invalid goals' if not check_goal(match_params[:redGoal], match_params[:blueGoal])
-    error[:player] = 'Duplicate players' if not check_players (players)
-    return error
+    error[:goal] = false if @goal_check
+    error[:player] = false if @player_check
+    error
   end
 
-  private
   def check_goal (goal1, goal2)
-     if goal1 == 10
-       goal2 == 10 ? false : true
-     end
-
-     if goal2 == 10
-       goal1 == 10 ? false : true
-     end
+    @goal_check = false if goal1 == 10 && goal2 < 10
+    @goal_check = false if goal2 == 10 && goal1 < 10
   end
 
-  private
   def check_players (players)
     players.each_with_index do |p|
       other_players = players.clone
       other_players.delete_at(players.index(p))
-      other_players.each {|op| return false if op == p}
-      true
+      other_players.each { |op| @player_check = true if op == p}
     end
   end
 
+  def check_for_error_params
+    error = {}
+    error[:goal] = 'Goals is invalid' if params.key?('goal')
+    error[:player] = 'Player is invalid' if params.key?('player')
+    error
+  end
 end
