07442c752abfe8cb297f48ebf69b307c70be4a42
Chester How <chesterhow@gmail.com>
2016-04-08 10:58:28 +0800

    Added validation for empty fields for matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -69,8 +69,16 @@ class MatchesController < ApplicationController
   end
 
   def validation
-    @goal_check = true
+    @selected_fields = 0;
+    @goal_check = false
     @player_check = false
+    @blanks = []
+
+    params_list = ['redAtt', 'redDef', 'blueAtt', 'blueDef', 'redGoal', 'blueGoal']
+
+    params_list.each do |p|
+      @selected_fields += 1 if check_selected(match_params[p], p)
+    end
 
     check_players([match_params[:redAtt], match_params[:redDef], match_params[:blueAtt], match_params[:blueDef]])
     check_goal(match_params[:redGoal].to_i, match_params[:blueGoal].to_i)
@@ -78,26 +86,41 @@ class MatchesController < ApplicationController
     error = {}
     error[:goal] = false if @goal_check
     error[:player] = false if @player_check
+    error[:blanks] = @blanks if @selected_fields < 6
     error
   end
 
+  def check_selected (field, name)
+    if field.present?
+      true
+    else
+      @blanks.push(name)
+      false
+    end
+  end
+
   def check_goal (goal1, goal2)
-    @goal_check = false if goal1 == 10 && goal2 < 10
-    @goal_check = false if goal2 == 10 && goal1 < 10
+    @goal_check = true if goal1 == 10 && goal2 == 10
+    @goal_check = true if goal1 < 10 && goal2 < 10
+
+    if goal1 == 'Score'|| goal2 == 'Score'
+      @goal_check = true
+    end
   end
 
   def check_players (players)
     players.each_with_index do |p|
       other_players = players.clone
       other_players.delete_at(players.index(p))
-      other_players.each { |op| @player_check = true if op == p}
+      @player_check = other_players.include?(p)
     end
   end
 
   def check_for_error_params
     error = {}
     error[:goal] = 'Goals is invalid' if params.key?('goal')
-    error[:player] = 'Player is invalid' if params.key?('player')
+    error[:player] = 'Duplicate players not allowed' if params.key?('player')
+    error[:blanks] = params[:blanks]
     error
   end
 end
